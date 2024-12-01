989e3ed483cd72dee3aaa3759c9fff41fd024e2e
ShaikJasmin23 <jasminshaik546@gmail.com>
2024-12-01 10:55:18 +0530

    Update admin.py

--- grievancepro/grievance/admin.py
+++ grievancepro/grievance/admin.py
@@ -13,6 +13,23 @@ admin.site.register(Complaint)
 
 
 
+from django.contrib import admin
+from .models import Grievance, Category, Department
+
+class GrievanceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'department', 'user', 'status', 'created_at')
+    list_filter = ('department', 'category', 'status')  # Add filters to make it easier to manage grievances
+
+admin.site.register(Grievance, GrievanceAdmin)
+admin.site.register(Category)
+admin.site.register(Department)
+
+
+
+
+
+
+
 
 
 
