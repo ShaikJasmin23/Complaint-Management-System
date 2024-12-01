ff6bffe0ad3dca51bac6f7972df4c8b0f3636bbc
ShaikJasmin23 <jasminshaik546@gmail.com>
2024-12-01 10:56:44 +0530

    Update forms.py

--- grievancepro/grievance/forms.py
+++ grievancepro/grievance/forms.py
@@ -57,6 +57,144 @@ class ComplaintForm(forms.ModelForm):
 
 
 
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.db import models
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, phone, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, phone=phone, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, phone, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, phone, password, **extra_fields)
+
+class User(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['phone']
+
+    def __str__(self):
+        return self.email
+
+
+
+
+
+
+from django import forms
+from .models import Grievance, Category, Department, GrievanceStatus
+
+class GrievanceForm(forms.ModelForm):
+    class Meta:
+        model = Grievance
+        fields = ['title', 'description', 'category', 'department', 'status']  # Include status in the form
+
+    # You can define custom widgets or queries for better select options, but this will show all departments and categories by default
+    department = forms.ModelChoiceField(queryset=Department.objects.all())
+    category = forms.ModelChoiceField(queryset=Category.objects.all())
+
+class StatusUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Grievance
+        fields = ['status']  # Only status is required to be updated
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.db import models
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, phone, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, phone=phone, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, phone, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, phone, password, **extra_fields)
+
+class User(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['phone']
+
+    def __str__(self):
+        return self.email
+
+
+
+
+
+
+from django import forms
+from .models import Grievance, Category, Department, GrievanceStatus
+
+class GrievanceForm(forms.ModelForm):
+    class Meta:
+        model = Grievance
+        fields = ['title', 'description', 'category', 'department', 'status']  # Include status in the form
+
+    # You can define custom widgets or queries for better select options, but this will show all departments and categories by default
+    department = forms.ModelChoiceField(queryset=Department.objects.all())
+    category = forms.ModelChoiceField(queryset=Category.objects.all())
+
+class StatusUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Grievance
+        fields = ['status']  # Only status is required to be updated
+
+
+
 
 
 
